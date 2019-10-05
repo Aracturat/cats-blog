9366bb58dabc658df84a2cff19572a90203567a8
Nikolay Dozmorov <ndozmorov@gmail.com>
2019-10-06 00:55:25 +0700

    Add generation of pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,14 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ]
 }
