3c251b334e83e43be6b59ef47a29d0572f97f4c1
Nikolay Dozmorov <ndozmorov@gmail.com>
2019-10-06 00:57:19 +0700

    Change cat template

--- src/templates/cat.js
+++ src/templates/cat.js
@@ -1,5 +1,30 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { Layout } from "../components/Layout"
 
-export default function () {
-  return (<div></div>)
+export default ({ data }) => {
+  const cat = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h1>{cat.frontmatter.title}</h1>
+        <img src={cat.frontmatter.image}
+          alt={cat.frontmatter.title}
+          style={{ width: 200, height: 200, objectFit: "cover" }}
+        />
+        <div dangerouslySetInnerHTML={{ __html: cat.html }} />
+      </div>
+    </Layout>
+  )
 }
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            html
+            frontmatter {
+                title
+                image
+            }
+        }
+    }
+`
